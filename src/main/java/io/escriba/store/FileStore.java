732a288df8a7e89ac5208faff8d4a3a2a85716b3
Ronaldo <ronaldo.silva@lemaf.ufla.br>
2016-06-21 18:16:30 -0300

    Draft!

--- src/main/java/io/escriba/store/FileStore.java
+++ src/main/java/io/escriba/store/FileStore.java
@@ -5,8 +5,14 @@ import java.io.File;
 public class FileStore extends Store {
 
 	private final File directory;
+	private final FileStore config;
 
 	public FileStore(File directory) {
+		this(directory, FileStoreConfig.DEFAULT);
+	}
+
+	public FileStore(File directory, FileStore config) {
 		this.directory = directory;
+		this.config = config;
 	}
 }
---
+++ src/main/java/io/escriba/store/FileStoreConfig.java
@@ -0,0 +1,5 @@
+package io.escriba.store;
+
+public class FileStoreConfig {
+	public static final FileStore DEFAULT = null;
+}
