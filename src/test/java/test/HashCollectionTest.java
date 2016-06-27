d6b2f7ae5ed5b581df3c53308e3b1ff6e4df96a0
Ronaldo <ronaldo.silva@lemaf.ufla.br>
2016-06-27 16:04:16 -0300

    Draft

--- src/test/java/test/HashCollectionTest.java
+++ src/test/java/test/HashCollectionTest.java
@@ -10,7 +10,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 public class HashCollectionTest {
 
-	@Test(timeOut = 100000L)
+	@Test(timeOut = 1000L)
 	public void insert01() throws Exception {
 		Store store = new Store(Help.newFile("mapdb", this.getClass()), Help.newDir("data", this.getClass()));
 
@@ -29,9 +29,8 @@ public class HashCollectionTest {
 
 		store.collection("col", true).put("val", (write, close) -> wait.attempt(() -> {
 				ByteBuffer buffer = ByteBuffer.allocate(1024 * 1024);
-				byte b = 0;
-				for (int i = 0; i < buffer.limit(); i++, b++) {
-					buffer.put(b);
+				for (int i = 0; i < buffer.limit(); i++) {
+					buffer.put((byte) i);
 				}
 				buffer.rewind();
 				write.apply(buffer, 0L);
