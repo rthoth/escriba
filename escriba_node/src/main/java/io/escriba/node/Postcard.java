9e2bf1ecf0c27ba439fdb9f976a03c8b24e0d30c
rthoth! <ronaldo.asilva@gmail.com>
2016-09-26 22:13:35 -0300

    Spaces

--- escriba_node/src/main/java/io/escriba/node/Postcard.java
+++ escriba_node/src/main/java/io/escriba/node/Postcard.java
@@ -10,6 +10,7 @@ public class Postcard implements Serializable {
 	private static final long serialVersionUID = 100L;
 
 	public final Anchor anchor;
+
 	public final String collection;
 
 	public Postcard(String collection, Server server) {
@@ -27,20 +28,6 @@ public class Postcard implements Serializable {
 		else
 			return new RemoteGet<>(node.bootstrap().clone(), this, key, initialSize, reader).future();
 	}
-
-//	public <T> Future<T> get(String key, int initialSize, PostcardReader<T> reader) throws Exception {
-//		if (isLocal())
-//			return new RemoteGet(this, key, initialSize, reader).future();
-//		else
-//			return new LocalGet(this, key, initialSize, reader).future();
-//	}
-
-//	public <T> Future<Postcard> put(String key, String mediaType, T content, PostcardWriter<T> writer) throws Exception {
-//		if (server == null)
-//			return new RemotePut<>(this, key, mediaType, content, writer).future();
-//		else
-//			return new LocalPut<>(this, key, mediaType, content, writer).future();
-//	}
 
 	protected <T, P> Future<Postcard> put(Node node, String key, String mediaType, P previous, T content, PostcardWriter<T, P> writer) throws Exception {
 
